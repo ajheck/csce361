797d3936971fa77bc2be6450c5f9f1095d7c0636
Andrew Heck <Andrew Heck>
2017-10-08 21:42:31 -0500

    Init of VS default ASP.NET solution

---
+++ Code/RoomQuery/RoomQuery/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RoomQuery.Startup))]
+namespace RoomQuery
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
