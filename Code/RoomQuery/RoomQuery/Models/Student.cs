1f06ecbbb1f048665440672426e6f50632b1042f
Andy Heck <andy_heck@hotmail.com>
2017-10-08 22:10:14 -0500

    Created Student.cs

---
+++ Code/RoomQuery/RoomQuery/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace RoomQuery.Models
+{
+    public class Student
+    {
+        [Key]
+        int StudentID { get; set; }
+        [Index(IsUnique = true)]
+        int Nuid { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        bool InSRC { get; set; }
+    }
+}
