7bee48e73d1533fbbae44d0a3b104335163f14ff
finneganhannah <hfinnegan8@gmail.com>
2017-10-26 20:59:59 -0500

    Added scan stuff

--- Code/RoomQuery/RoomQuery/Controllers/DataController.cs
+++ Code/RoomQuery/RoomQuery/Controllers/DataController.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RoomQuery.DataCollectionBuisnessLayer;
 
 namespace RoomQuery.Controllers
 {
     public class DataController : Controller
     {
-        // GET: Data
+        private string model;
+
         public ActionResult InsertData()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Submit(string nuid)
+        {
+            DataService DataService = new DataService();
+            DataService.HandleScan(nuid);
+
+            return View("InsertData", model);
         }
     }
 }
---
+++ Code/RoomQuery/RoomQuery/DataCollectionBuisnessLayer/DataService.cs
@@ -0,0 +1,40 @@
+﻿using RoomQuery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RoomQuery.DataCollectionBuisnessLayer
+{
+    public class DataService
+    {
+        private ApplicationDbContext Context = ApplicationDbContext.Create();
+
+        public IEnumerable<Student> GetStudents()
+        {
+            return Context.Students.Where(x => x != null);
+        }
+
+        public IEnumerable<Student> GetCurrentStudents()
+        {
+            return this.GetStudents().Where(x => x.InSRC == true);
+        }
+
+        public void HandleScan(String n)
+        {
+            var student = this.GetStudents().FirstOrDefault(x => x.Nuid == n);
+            student.InSRC = !student.InSRC;
+            this.GetStudents().FirstOrDefault(x => x.StudentID == student.StudentID).InSRC = student.InSRC;
+
+            this.Context.SaveChanges();
+
+            SRCTimestamp newStamp = new SRCTimestamp();
+            newStamp.Stamp = DateTime.Now;
+            newStamp.Student = student;
+            newStamp.WasCheckIn = student.InSRC;
+            this.Context.Timestamps.Add(newStamp);
+
+            this.Context.SaveChanges();
+        }
+    }
+}
